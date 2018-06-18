1793584071ac8b5d37789763b1923c1d47040e35
hp probook 242 g1 <apps.selusin@gmail.com>
2018-06-18 15:08:58 +0700

    1

--- ewcfg14.php
+++ ewcfg14.php
@@ -25,7 +25,7 @@ define("EW_CONFIG_FILE_FOLDER", EW_PROJECT_NAME, TRUE); // Config file name
 define("EW_PROJECT_ID", "{EA1CE07B-E03E-4EC9-BC42-48D490D73F97}", TRUE); // Project ID (GUID)
 $EW_RELATED_PROJECT_ID = "";
 $EW_RELATED_LANGUAGE_FOLDER = "";
-define("EW_RANDOM_KEY", 'oKm2nv1ZAY6k64qP', TRUE); // Random key for encryption
+define("EW_RANDOM_KEY", 'sJpBw3bewmLmOMI0', TRUE); // Random key for encryption
 define("EW_PROJECT_STYLESHEET_FILENAME", "phpcss/prj_anggaran.css", TRUE); // Project stylesheet file name
 define("EW_CHARSET", "utf-8", TRUE); // Project charset
 define("EW_EMAIL_CHARSET", EW_CHARSET, TRUE); // Email charset
--- ewmenu.php
+++ ewmenu.php
@@ -3,6 +3,8 @@
 // Menu
 $RootMenu = new cMenu("RootMenu", TRUE);
 $RootMenu->AddMenuItem(2, "mi_cf01_home_php", $Language->MenuPhrase("2", "MenuText"), "cf01_home.php", -1, "", TRUE, FALSE, TRUE, "");
+$RootMenu->AddMenuItem(10007, "mi_t98_log", $Language->MenuPhrase("10007", "MenuText"), "t98_loglist.php", 2, "", TRUE, FALSE, FALSE, "");
+$RootMenu->AddMenuItem(10008, "mi_t99_log_status", $Language->MenuPhrase("10008", "MenuText"), "t99_log_statuslist.php", 2, "", TRUE, FALSE, FALSE, "");
 $RootMenu->AddMenuItem(1, "mi_t01_penerimaan", $Language->MenuPhrase("1", "MenuText"), "t01_penerimaanlist.php", -1, "", TRUE, FALSE, FALSE, "");
 $RootMenu->AddMenuItem(10005, "mi_t03_pengeluaran_head", $Language->MenuPhrase("10005", "MenuText"), "t03_pengeluaran_headlist.php", -1, "", TRUE, FALSE, FALSE, "");
 $RootMenu->AddMenuItem(10006, "mi_t04_satuan", $Language->MenuPhrase("10006", "MenuText"), "t04_satuanlist.php", -1, "", TRUE, FALSE, FALSE, "");
